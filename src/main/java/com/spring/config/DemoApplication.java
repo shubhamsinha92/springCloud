961db48284c97beadc22f1ba4ed156da6d7efd79
Shubham Sinha <shubham.sinha@berkeley.edu>
2017-06-13 20:41:52 -0500

    -git configuration

--- src/main/java/com/spring/config/DemoApplication.java
+++ src/main/java/com/spring/config/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.spring.config;
 
-import javax.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -12,13 +10,5 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Value("${eviveKeys}")
-	String password;
-
-	@PostConstruct
-	private void postConstruct() {
-		System.out.println("My password is: " + password);
 	}
 }
